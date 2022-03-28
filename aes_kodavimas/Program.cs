5a03a9145a48f35975f7fe7312bc4bf4744ffdc9
donatas <donatas@DESKTOP-SKDD>
2022-03-28 09:18:18 +0300

    Add project files.

---
+++ aes_kodavimas/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace cryptografija
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ka norite daryt ?: 1- uzsifruoti 2-desifuoriti");
+            string pasirinkimas = Console.ReadLine();
+            if (pasirinkimas == "1")
+            {
+                Console.WriteLine("irasykite slaptazodi");
+                string password = Console.ReadLine();
+                FileEncrypt("Orginalas.txt", password);
+
+            }
+            if (pasirinkimas == "2")
+            {
+                Console.WriteLine("irasykite slaptazodi");
+                string password = Console.ReadLine();
+                FileDecrypt("Orginalas.txt.aes", "Desifruotas.txt", password);
+            }
+            Console.WriteLine("Pavyko!");
+            Console.ReadLine();
+        }
+
+        public static byte[] GenerateRandomSalt()
+        {
+            byte[] data = new byte[32];
+
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    rng.GetBytes(data);
+                }
+            }
+
+            return data;
+        }
+
+        private static void FileEncrypt(string inputFile, string password)
+        {
+            byte[] salt = GenerateRandomSalt();
+
+            FileStream fsCrypt = new FileStream(inputFile + ".aes", FileMode.Create);
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            AES.Padding = PaddingMode.PKCS7;
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Mode = CipherMode.CBC;
+            fsCrypt.Write(salt, 0, salt.Length);
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write);
+            FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+            byte[] buffer = new byte[1048576];
+            int read;
+            try
+            {
+                while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    cs.Write(buffer, 0, read);
+                }
+                fsIn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Nepavyko: " + ex.Message);
+            }
+            finally
+            {
+                cs.Close();
+                fsCrypt.Close();
+            }
+        }
+
+        private static void FileDecrypt(string inputFile, string outputFile, string password)
+        {
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] salt = new byte[32];
+            FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+            fsCrypt.Read(salt, 0, salt.Length);
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Padding = PaddingMode.PKCS7;
+            AES.Mode = CipherMode.CBC;
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read);
+            FileStream fsOut = new FileStream(outputFile, FileMode.Create);
+            int read;
+            byte[] buffer = new byte[1048576];
+
+            try
+            {
+                while ((read = cs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    fsOut.Write(buffer, 0, read);
+                }
+            }
+            catch (CryptographicException ex_CryptographicException)
+            {
+                Console.WriteLine("Nepavyko " + ex_CryptographicException.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Nepavyko " + ex.Message);
+            }
+
+            try
+            {
+                cs.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Nepavyko: " + ex.Message);
+            }
+            finally
+            {
+                fsOut.Close();
+                fsCrypt.Close();
+            }
+        }
+
+
+    }
+}
